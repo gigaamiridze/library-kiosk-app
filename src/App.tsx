ef657c6fbe3637e91be907bf904ea82a04dfc2b0
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-19 18:23:59 +0400

    Modified App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { NavigationContainer } from '@react-navigation/native';
+import { AlertNotificationRoot } from 'react-native-alert-notification';
+import { UserContextProvider, LibraryContextProvider } from './contexts';
+import { StackNavigation } from './navigation';
+
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text>Library Kiosk</Text>      
-    </SafeAreaView>
+    <UserContextProvider>
+      <LibraryContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <AlertNotificationRoot>
+            <NavigationContainer>
+              <StackNavigation />
+            </NavigationContainer>
+          </AlertNotificationRoot>
+        </QueryClientProvider>
+      </LibraryContextProvider>    
+    </UserContextProvider>
   );
 }
 
