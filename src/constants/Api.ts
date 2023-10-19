45b6951487fef99a724a803e8e2b9f28157f32b5
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-19 18:12:08 +0400

    Added constants folder

---
+++ src/constants/Api.ts
@@ -0,0 +1,7 @@
+import { BOOKS_API_KEY } from '@env';
+
+export const Api = {
+  KEY: BOOKS_API_KEY,
+  AUTH_URL: 'https://dummyjson.com/auth/login',
+  BOOKS_URL: 'https://www.googleapis.com/books/v1',
+}
